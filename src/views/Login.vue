ddf6dc2dbb7581fceea9408fc787f57a6a3b6ce3
Kevin Potts <skydivekevin@gmail.com>
2018-10-26 14:09:25 -0600

    write review view working now; everything else works as well
    Signed-off-by: Kevin Potts <skydivekevin@gmail.com>

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -20,7 +20,6 @@
                       placeholder="Enter password">
         </b-form-input>
       </b-form-group>
-
       </b-form-group>
       <b-button type="submit" variant="primary">Submit</b-button>
       <b-button type="reset" variant="outline-primary">Reset</b-button>
--- src/views/Writereview.vue
+++ src/views/Writereview.vue
@@ -1,16 +1,38 @@
 <template>
   <div>
-    <h1>Write a review</h1>
-      <div>
-        <b-form-textarea id="textarea1"
-          v-model="text"
-          placeholder="Enter something"
-          :rows="3"
-          :max-rows="6">
-        </b-form-textarea>
-      <pre class="mt-3">{{ text }}</pre>
-  </div>
+    <b-form @submit="onSubmit" @reset="onReset" v-if="show">
+      <b-form-group id="dropzoneGroup"
+        label="Dropzone:"
+        label-for="dropzone">
+        <b-form-select id="dropzone"
+          :options="dropzones"
+          required
+          v-model="form.dropzone">
+        </b-form-select>
+      </b-form-group>
+      <b-form-group id="instructorGroup"
+        label="Instructor:"
+        label-for="instructor">
+        <b-form-select id="instructor"
+          :options="instructors"
+          required
+          v-model="form.instructor">
+        </b-form-select>
+      </b-form-group>
 
+    <b-form-textarea id="review"
+      v-model="form.review"
+      placeholder="Write review here"
+      :rows="3"
+      :max-rows="6"
+      >
+    </b-form-textarea>
+    <pre class="mt-3">{{ review }}</pre>
+
+        <b-button type="submit" variant="primary">Submit</b-button>
+      <b-button type="reset" variant="outline-primary">Reset</b-button>
+      
+    </b-form>
   </div>
 </template>
 
@@ -18,18 +40,49 @@
 export default {
   data() {
     return {
-      text: ''
+      form: {
+        dropzone: null,
+        instructor: null,
+        review: null
+      },
+      dropzones: [
+        { text: 'Select One', value: null },
+        'Mile-Hi',
+        'Skydive Hawaii',
+        'Skydive Cumberland',
+        'Skydive Galveston'
+      ],
+      instructors: [
+        { text: 'Select your instructor', value: null },
+        'Kevin Potts',
+        'Jeff Stagg',
+        'Brian Moler',
+        'John Wheldon'
+      ],
+      review: '',
+      show: true
     };
+  },
+  methods: {
+    onSubmit(evt) {
+      evt.preventDefault();
+      alert(JSON.stringify(this.form));
+    },
+    onReset(evt) {
+      evt.preventDefault();
+      /* Reset our form values */
+      this.form.dropzone = null;
+      this.form.instructor = null;
+      this.form.review = null;
+      /* Trick to reset/clear native browser form validation state */
+      this.show = false;
+      this.$nextTick(() => {
+        this.show = true;
+      });
+    }
   }
 };
 </script>
-
-
-<style>
-</style>
-
-
-
 
 
 
